825142c07e1ab3e15dc0cd3343f5f647405b5fcf
dizolo <y20072004@gmail.com>
2022-11-12 15:38:05 +0800

    fix

--- src/store/api.js
+++ src/store/api.js
@@ -2,11 +2,11 @@ import Api, {AuthApi, ScheduleTrackingApi, TrackingApi, UserApi} from "@/api";
 
 export default {
     state: {
-        authApi: new AuthApi(),
-        trackingApi: new TrackingApi(),
-        scheduleTrackingApi: new ScheduleTrackingApi(),
-        userApi: new UserApi(),
-        api: new Api(new AuthApi(), new TrackingApi(), new ScheduleTrackingApi(), new UserApi())
+        authApi: new AuthApi(process.env.backendUrl),
+        trackingApi: new TrackingApi(process.env.backendUrl),
+        scheduleTrackingApi: new ScheduleTrackingApi(process.env.backendUrl),
+        userApi: new UserApi(process.env.backendUrl),
+        api: new Api(new AuthApi(process.env.backendUrl), new TrackingApi(process.env.backendUrl), new ScheduleTrackingApi(process.env.backendUrl), new UserApi(process.env.backendUrl))
     },
 
 }
