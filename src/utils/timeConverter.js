1b0d887fa01ab81fa85c8c6ef0bc45d7b3dac223
dizolo <y20072004@gmail.com>
2023-01-04 12:13:54 +0800

    fix timeConverter.js

--- src/utils/timeConverter.js
+++ src/utils/timeConverter.js
@@ -1,9 +1,8 @@
-
 const datetime = require('date-and-time');
 
 
-export default class TimeConverter{
-    humanizeTime(time){
-        return datetime.format(new Date(time*1000),"DD.MM.YYYY HH:mm",true)
+export default class TimeConverter {
+    humanizeTime(time) {
+        return datetime.format(new Date(time), "DD.MM.YYYY HH:mm", true)
     }
 }
