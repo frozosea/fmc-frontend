bed9e4681c575123fffa3001a3f341778ea687f6
dizolo <y20072004@gmail.com>
2023-05-19 16:54:40 +0800

    add yandex analytics

--- src/main.js
+++ src/main.js
@@ -4,16 +4,20 @@ import components from '@/UI';
 import {VueSpinners} from '@saeris/vue-spinners'
 import router from "@/router/router";
 import store from "@/store";
+import VueYandexMetrika from 'vue-yandex-metrika'
 
 const app = createApp(App)
 
-// createApp(App).mount('#app')
 
 components.forEach(component => {
     app.component(component.name, component)
 })
 
-app.use(VueSpinners).use(router).use(store).mount("#app")
+app.use(VueSpinners).use(router).use(store).use(VueYandexMetrika, {
+    id: 93636620,
+    router: router,
+    env: process.env.NODE_ENV,
+}).mount("#app")
 
 
 
